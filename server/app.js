808cffc1727e02ac8513dee9a202bfbc6fe887b2
Daniel Kim <dannykim200@gmail.com>
2021-03-13 15:19:56 -0500

    setting up graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({
+
+})); // handles graphql request
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
