61abfd0a58e7e7e204e103568a739ff2e42917e4
Daniel Kim <dannykim200@gmail.com>
2021-03-14 20:00:34 -0400

    type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,9 +5,9 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt }
 
 // dummy data
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
 ];
 
 var authors = [
@@ -22,6 +22,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      // return the author
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   }),
 });
 
