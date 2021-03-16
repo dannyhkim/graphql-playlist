d4ec392d9a7b96a7646027770533ab1c0175daff
Daniel Kim <dannykim200@gmail.com>
2021-03-15 21:59:52 -0400

    connecting to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mongodb database
+mongoose.connect("mongodb+srv://daniel:test123@gql-ninja.ilbfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+
+  console.log('connected to db');
+});
 
 app.use("/graphql", graphqlHTTP({
   schema,
