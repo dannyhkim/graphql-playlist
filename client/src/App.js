9fbd1d6146e02385f34e510c894191c87ead25b4
Daniel Kim <dannykim200@gmail.com>
2021-03-17 20:01:33 -0400

    apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import BookList from './components/BookList';
+import React, { Component } from "react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+// components
+import BookList from "./components/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql", // making requests to this endpoint
+  cache: new InMemoryCache(),
+});
 
 class App extends Component {
-
   render() {
     return (
-      <div id="main">
-        <h1>Ninja's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
